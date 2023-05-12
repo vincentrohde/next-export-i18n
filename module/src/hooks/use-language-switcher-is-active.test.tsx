4f56443fbd77edef8796c6ab9a1f006e23e228bd
Vincent Rohde <vincent.rohde@icloud.com>
2023-05-13 00:49:17 +0200

    feat(#51): add tests for useLanguageSwitcherIsActive

--- module/src/hooks/use-language-switcher-is-active.test.tsx
+++ module/src/hooks/use-language-switcher-is-active.test.tsx
@@ -3,16 +3,16 @@
  */
 import { cleanup, renderHook } from '@testing-library/react';
 import useLanguageSwitcherIsActive from './use-language-switcher-is-active';
+import { LanguageDataStore } from '../enums/languageDataStore';
 
-jest.mock('./../../../i18n/index', () => {
-	return {
-		__esModule: true,
-		i18n: {
-			translations: { mock: { title: 'mock' }, foo: { title: 'bar' } },
-			defaultLang: 'mock',
-		},
-	};
-});
+const mockConfig = {
+  translations: {
+    mock: { title: 'mock' },
+    foo: { title: 'bar' },
+  },
+  defaultLang: 'mock',
+  useBrowserDefault: true,
+}
 
 jest.mock('next/router', () => ({
 	useRouter() {
@@ -32,26 +32,21 @@ afterEach(() => {
 	jest.clearAllMocks();
 });
 
-describe('The hook returns ', () => {
-  afterEach(() => {
-    cleanup();
-    jest.clearAllMocks();
-  });
+describe('Query: The hook returns ', () => {
+  beforeAll(() => {
+    jest.spyOn(require('./../index'), 'i18n').mockReturnValue({
+      ...mockConfig,
+      languageDataStore: LanguageDataStore.QUERY
+    })
+  })
 
-	it(`true if there is no query at the router and the param equals the default language`, async () => {
+	it(`true if there is no query at the router and the param equals the default language `, async () => {
 		useRouter.mockImplementation(() => ({
 			query: null,
 		}));
     const { result } = renderHook(() => useLanguageSwitcherIsActive('mock'));
 		expect(result.current.isActive).toBe(true);
 	});
-
-  it(`true if there is no lang at the localStorage and the param equals the default language`, async () => {
-    jest.requireMock('./../../../i18n/index').i18n.languageDataStore = 'localStorage';
-    jest.spyOn(window.localStorage, 'getItem').mockReturnValue(null);
-    const { result } = renderHook(() => useLanguageSwitcherIsActive('mock'));
-    expect(result.current.isActive).toBe(true);
-  });
 
 	it(`false if there is no query at the router and the param does not equal the default language `, async () => {
 		useRouter.mockImplementation(() => ({
@@ -60,6 +55,7 @@ describe('The hook returns ', () => {
 		const { result } = renderHook(() => useLanguageSwitcherIsActive('foo'));
 		expect(result.current.isActive).toBe(false);
 	});
+
 	it(`true if there is no language at the query and the param equals the default language `, async () => {
 		useRouter.mockImplementation(() => ({
 			query: {},
@@ -76,7 +72,7 @@ describe('The hook returns ', () => {
 		expect(result.current.isActive).toBe(false);
 	});
 
-	it(`true if the router query equals the param`, async () => {
+	it(`true if the router query equals the param `, async () => {
 		useRouter.mockImplementation(() => ({
 			query: { lang: 'foo' },
 		}));
@@ -84,11 +80,42 @@ describe('The hook returns ', () => {
 		expect(result.current.isActive).toBe(true);
 	});
 
-	it(`false if the router does not query equals the param`, async () => {
+	it(`false if the router query does not equal the param`, async () => {
 		useRouter.mockImplementation(() => ({
 			query: { lang: 'foo' },
 		}));
 		const { result } = renderHook(() => useLanguageSwitcherIsActive('bar'));
 		expect(result.current.isActive).toBe(false);
 	});
+});
+
+describe('Local Storage: The hook returns ', () => {
+  beforeAll(() => {
+    jest.spyOn(require('./../index'), 'i18n').mockReturnValue({
+      ...mockConfig,
+      languageDataStore: LanguageDataStore.LOCAL_STORAGE
+    })
+  })
+
+  it(`true if there is no data at the local storage and the param equals the default language`, async () => {
+    const { result } = renderHook(() => useLanguageSwitcherIsActive('mock'));
+    expect(result.current.isActive).toBe(true);
+  });
+
+  it(`false if there is no data at the local storage and the param does not equal the default language`, async () => {
+    const { result } = renderHook(() => useLanguageSwitcherIsActive('foo'));
+    expect(result.current.isActive).toBe(false);
+  });
+
+  it(`true if the local storage value equals the param`, async () => {
+    window.localStorage.setItem('lang', 'foo');
+    const { result } = renderHook(() => useLanguageSwitcherIsActive('foo'));
+    expect(result.current.isActive).toBe(true);
+  });
+
+  it(`false if the local storage does not equal the param`, async () => {
+    window.localStorage.setItem('lang', 'foo');
+    const { result } = renderHook(() => useLanguageSwitcherIsActive('mock'));
+    expect(result.current.isActive).toBe(false);
+  });
 });
